4d5ac7644b829de02512860be40b6a4c1168d0c1
MuhammadHammad001 <hammad.bashir@10xengineers.ai>
2023-08-30 18:09:13 +0500

    Testbench for multiplier updated

--- test/tbMultiplier.sv
+++ test/tbMultiplier.sv
@@ -1,7 +1,26 @@
+//////////////////////////////////////////////////////////////////////
+// _______        _   _                     _     					//
+// |__   __|      | | | |                   | |    					//
+//    | | ___  ___| |_| |__   ___ _ __   ___| |__  					//
+//    | |/ _ \/ __| __| '_ \ / _ \ '_ \ / __| '_ \ 					//
+//    | |  __/\__ \ |_| |_) |  __/ | | | (__| | | |					//
+//    |_|\___||___/\__|_.__/ \___|_| |_|\___|_| |_|					//
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//                                                                  //
+//  Author: Muhammad Hammad Bashir                                  //
+//  Description:                                                    //
+//			This file contains the testbench for the Multiplier, I  //
+//			have tried multiple numbers for the multiplication which//
+//			you may refer to below.clk								//
+//////////////////////////////////////////////////////////////////////
+												
+
+
 module tbMultiplier();
-logic [15:0]x,y;
-logic start,clk,rst;
-logic [31:0]mult;
+	logic [15:0]	x,y;
+	logic 			start,clk,rst;
+	logic [31:0]	mult;
 
 Multiplier MUL(.x(x),.y(y),.start(start),.clk(clk),.rst(rst),.mult(mult));
 
